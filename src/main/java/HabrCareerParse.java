1935096c0d79e1b79f2b195d537204e0102d3b2f
spoletaev <info.elekit@yandex.ru>
2022-04-19 14:41:16 +0300

    2.4. SqlRuParse [#285213]. вынесено получение Post  в отдельный метод, исправлена логика метода list.

--- src/main/java/HabrCareerParse.java
+++ src/main/java/HabrCareerParse.java
@@ -21,13 +21,9 @@ public class HabrCareerParse implements Parse {
         this.dataTimeParser = dataTimeParser;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         HabrCareerParse careerParse = new HabrCareerParse(new HabrCareerDataTimeParser());
-        List<Post> rslPosts = new ArrayList<>();
-        for (int i = 1; i <= 5; i++) {
-            List<Post> posts = careerParse.list(PAGE_LINK + "?page=" + i);
-            rslPosts.addAll(posts);
-        }
+        List<Post> rslPosts = careerParse.list(PAGE_LINK);
         rslPosts.forEach(System.out::println);
     }
 
@@ -39,27 +35,37 @@ public class HabrCareerParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        List<Post> posts = new ArrayList<>();
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element vacancyDate = dateElement.child(0);
-            String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            LocalDateTime vacancyCreated = dataTimeParser.parse(vacancyDate.attr("datetime"));
-            String vacancyDescription = "";
+    public List<Post> list(String link) {
+        List<Post> rslPosts = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
             try {
-                vacancyDescription = retrieveDescription(vacancyLink);
+                Connection connection = Jsoup.connect(link + "?page=" + i);
+                Document document = connection.get();
+                List<Post> posts = new ArrayList<>();
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> posts.add(getPost(row)));
+                rslPosts.addAll(posts);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            posts.add(new Post(vacancyName, vacancyLink, vacancyDescription, vacancyCreated));
-        });
-        return posts;
+        }
+        return rslPosts;
+    }
+
+    private Post getPost(Element row) {
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        String vacancyName = titleElement.text();
+        Element dateElement = row.select(".vacancy-card__date").first();
+        Element vacancyDate = dateElement.child(0);
+        String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        LocalDateTime vacancyCreated = dataTimeParser.parse(vacancyDate.attr("datetime"));
+        String vacancyDescription = "";
+        try {
+            vacancyDescription = retrieveDescription(vacancyLink);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Post(vacancyName, vacancyLink, vacancyDescription, vacancyCreated);
     }
 }
